2142cca4cf33cd079c9b57d6463e68948592e4bb
kennylisal <kennylisal5@gmail.com>
2025-08-21 12:55:13 +0800

    added authorization-services implementation

--- src/infrastructure/services/authorization.services.ts
+++ src/infrastructure/services/authorization.services.ts
@@ -4,6 +4,42 @@
 //   });
 // }
 
+import { IAuthorizationServices } from "@/application/services/authorization.service.interface";
+import { AuthorizationError } from "@/entities/error/common";
+import { DOMAIN_MEMBERSHIP_ROLE } from "@/entities/models/domain-membership";
+import { ACCOUNT_ROLE } from "@/entities/models/user";
+import { UseCaseAction, RESOURCE } from "@/entities/models/usercase-actions";
+export class AuthorizationServices implements IAuthorizationServices {
+  isActionPermitted(action: UseCaseAction, usedId: string): void {
+    throw new Error("Method not implemented.");
+  }
+  isAuthorizedToCreateOrUpdateOrDelete(
+    resource: RESOURCE,
+    role: DOMAIN_MEMBERSHIP_ROLE
+  ): void {
+    if (
+      role !== DOMAIN_MEMBERSHIP_ROLE.MEMBER &&
+      role !== DOMAIN_MEMBERSHIP_ROLE.MODERATOR
+    ) {
+      throw new AuthorizationError(
+        "Only members or moderators are permitted to perform this action"
+      );
+    }
+  }
+  isAuthorizedToRead(resource: RESOURCE, resourceId: number): void {
+    if (resource !== RESOURCE.POST) {
+      throw new AuthorizationError("Only Posts are free to read");
+    }
+  }
+  isAuthorizedForAdministrationalAction(role: ACCOUNT_ROLE): void {
+    if (role === ACCOUNT_ROLE.STUDENT) {
+      throw new AuthorizationError(
+        "Only teachers are permitted to do administration actions"
+      );
+    }
+  }
+}
+
 //   if (!isAuthorized) {
 //     throw new AuthorizationError("Not Authorized for admin action", {
 //       cause: `User role is ${userRole}`,
